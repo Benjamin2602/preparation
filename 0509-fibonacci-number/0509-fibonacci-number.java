1abfdafcc14f22ee9f2337f764a3e7a4e4cad26f
Benjamin samuel <91728550+Benjamin2602@users.noreply.github.com>
2023-12-28 09:42:23 +0530

    Time: 94 ms (8.63%), Space: 44.5 MB (5.54%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,9 +1,18 @@
 class Solution {
-    public int fib(int n) {
+    // dynamic programming
+    public int fib(int n){
+        return fib(n,new HashMap<>());
+    }
+    public int fib(int n , HashMap<Integer, Integer> memo) {
         if(n==0 || n==1){
             return n;
         }
-        return fib(n-1) + fib(n-2);
+        if(memo.containsKey(n)){
+            return memo.get(n); 
+        }
+        int res = fib(n-1) + fib(n-2);
+        memo.put(n,res);
+        return res;
         
     }
 }
