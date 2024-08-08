e40897d5e393fbce75db476e0074f31840154815
Benjamin samuel <91728550+Benjamin2602@users.noreply.github.com>
2024-08-08 11:49:48 +0530

    Time: 1 ms (99.53%), Space: 57 MB (52.63%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -3,16 +3,8 @@ class Solution {
         int currMax = 0;
         int maximum = Integer.MIN_VALUE;
         for(int i = 0; i<nums.length; i++){
-            int temp = currMax + nums[i];
-            if(temp<nums[i]){
-                currMax = nums[i];
-            }
-            else{
-                currMax = temp;
-            }
-            if(currMax > maximum){
-                maximum = currMax;
-            }
+            currMax = Math.max(nums[i],nums[i]+currMax);
+            maximum = Math.max(currMax,maximum);
         }
         return maximum;
         
