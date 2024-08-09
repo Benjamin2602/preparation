29fe92397e3146af53166c6a9bcb007b60f7d85e
Benjamin samuel <91728550+Benjamin2602@users.noreply.github.com>
2024-08-09 11:10:46 +0530

    Time: 6 ms (68.78%), Space: 44.7 MB (30.85%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,21 +1,29 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        //sliding window approach
-        int left = 0;
-        int right =0;
-        int max = 0;
-        HashSet<Character>set = new HashSet<>();
-        while(right<s.length()){
-            if(set.contains(s.charAt(right))){
-                set.remove(s.charAt(left));
-                left++;  // removing the duplicates
-            }else{
-                set.add(s.charAt(right));
-                right++;
-                max = Math.max(set.size(),max);
+        int start = 0;
+        int end = 0;
+        // if any ""string it returns 0 or if any string then 1
+        int maxLen = Math.min(s.length(),1);
+        HashSet<Character> seen = new HashSet<>();
+        while(end < s.length()){
+            // if the element already contains for example
+            //in hashset <a,b,c> is there and the fourth char is a 
+            // so need to remove the char which points at the start
+            // <b,c,a> incremeting the start pointer to s.length(1)
+            if(seen.contains(s.charAt(end))){
+                seen.remove(s.charAt(start));
+                start++;
+            }
+            else{
+                //if no element contains then move the end pointer
+                // add new element to the hashset 
+                //<a>, <a,b> , <a,b,c>
+                seen.add(s.charAt(end));
+                int windowSize = end - start + 1;
+                maxLen = Math.max(windowSize,maxLen);
+                end++;
             }
         }
-        return max;
-        
+        return maxLen;
     }
 }
